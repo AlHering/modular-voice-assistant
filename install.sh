691623a078e04779d27e9fde7c82aa20d0ad0a38
Alexander Hering <dev.hering@gmail.com>
2024-04-30 21:26:47 +0200

    refactored docker base

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 printf "\n%s\n" "${delimiter}"
-printf "Activating conda environment"
+printf "Activating environment"
 printf "\n%s\n" "${delimiter}"
  
-if [[ -f "${CONDA_DIR}/etc/profile.d/conda.sh"  ]]
+if [[ -f "${SCRIPT_DIR}/venv/bin/activate" ]]
 then
-    source "${CONDA_DIR}/etc/profile.d/conda.sh"
+    source "${SCRIPT_DIR}/venv/bin/activate"
 else
     printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: Cannot initialize conda, aborting...\e[0m"
+    printf "\e[1m\e[31mERROR: Cannot activate environment, aborting...\e[0m"
     printf "\n%s\n" "${delimiter}"
     exit 1
 fi 
 
 printf "\n%s\n" "${delimiter}"
-printf "Setting up conda environment..."
-printf "\n%s\n" "${delimiter}"
-conda activate "$VENV_DIR"
-conda install -y -k  $CONDA_TORCH_CUDA_INSTALLATION
-
-printf "\n%s\n" "${delimiter}"
-printf "Handling main webui requirements..."
+printf "Handling main requirements..."
 printf "\n%s\n" "${delimiter}"
 
-python -m pip install -r requirements.txt
-#CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 python -m pip install --upgrade llama-cpp-python
+if [[ $CUDA_SUPPORT == "True" ]]
+then
+    source "${SCRIPT_DIR}/venv/bin/activate" && pip install --no-cache-dir -r ${SCRIPT_DIR}/requirements_gpu.txt
+else
+    source "${SCRIPT_DIR}/venv/bin/activate" && pip install --no-cache-dir -r ${SCRIPT_DIR}/requirements_cpu.txt
+fi 
 
 printf "\n%s\n" "${delimiter}"
 printf "Finished installation"
