bd6bf9f1d70ba2d3e1618c90991f6efc3062c119
Alexander Hering <dev.hering@gmail.com>
2024-05-01 14:28:26 +0200

    starting stt handler class for testing

---
+++ src_prototype/speech_to_text.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*             Modular Voice Assistant              *
+*            (c) 2024 Alexander Hering             *
+****************************************************
+"""
+
+
+class STTHandler(object):
+    """
+    Class for representing Speech To Text handlers.
+    """
+    def __init__(self) -> None:
+        """
+        Initiation method.
+        """
+        pass
