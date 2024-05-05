34e628f55e4dfb06267ba5f7cd8adc20ec2734da
Alexander Hering <dev.hering@gmail.com>
2024-05-05 19:48:57 +0200

    added tts model creation and synthesis to file

--- src/utility/gold/sound_generation/text_to_speech_utility.py
+++ src/utility/gold/sound_generation/text_to_speech_utility.py
@@ -5,3 +5,96 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
+from typing import Any
+import os
+import pyaudio
+import wave
+import torch
+from TTS.api import TTS
+
+
+TEMPORARY_DATA_FOLDER = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data")
+if not os.path.exists:
+     os.makedirs(TEMPORARY_DATA_FOLDER)
+TEMPORARY_OUTPUT_PATH = os.path.join(TEMPORARY_DATA_FOLDER, "out.wav")
+
+DEFAULT_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def play_wave(wave_file: str, chunk_size: int = 1024, stream_kwargs: dict = None) -> None:
+    """
+    Plays wave audio file.
+    :param wave_file: Wave file path.
+    :param chunk_size: Chunk size for file handling. 
+        Defaults to 1024.
+    :param stream_kwargs: Stream keyword arguments.
+        Defaults to None in which case
+        {
+            "rate": output_file.getnchannels(),
+            "format": pya.get_format_from_width(output_file.getsampwidth()),
+            "channels": output_file.getnchannels(),
+            "output": True
+        } is used.
+    """
+    pya = pyaudio.PyAudio()
+    output_file = wave.open(wave_file, "rb")
+    stream_kwargs = {
+        "rate": output_file.getnchannels(),
+        "format": pya.get_format_from_width(output_file.getsampwidth()),
+        "channels": output_file.getnchannels(),
+        "output": True
+    } if stream_kwargs is None else stream_kwargs
+    stream = pya.open(
+        **stream_kwargs
+    )
+    data = output_file.readframes(chunk_size)
+    while data != "":
+        stream.write(data)
+        data = output_file.readframes(chunk_size)
+    stream.stop_stream()
+    stream.close()
+    pya.terminate()
+
+
+def get_tts_model(model_name_or_path: str, instantiation_kwargs: dict = None) -> TTS:
+    """
+    Returns a TTS based model instance.
+    :param model_name_or_path: Model name or path.
+    :param instantiation_kwargs: Instatiation keyword arguments.
+        Defaults to None in which case default values are used.
+    :returns: TTS model instance.
+    """
+    instantiation_kwargs = {} if instantiation_kwargs is None else instantiation_kwargs
+    if os.path.exists(model_name_or_path):
+        return TTS().load_tts_model_by_path(
+            model_path=model_name_or_path,
+            config_path=f"{model_name_or_path}/config.json",
+            **instantiation_kwargs)
+    else:
+         return TTS(
+              model_name=model_name_or_path,
+              **instantiation_kwargs
+         )
+
+
+def synthesize_with_tts_to_file(text: str, output_path: str = None, model: TTS = None, synthesis_kwargs: dict = None) -> str:
+    """
+    Synthesizes text with TTS and saves results to a file.
+    :param text: Output text.
+    :param output_path: Output path.
+        Defaults to None in which case the file "out.wav" under the temporary data folder is used.
+    :param model: TTS model. 
+        Defaults to None in which case a default model is instantiated and used.
+        Not providing a model therefore increases processing time tremendously!
+    :param synthesis_kwargs: Synthesis keyword arguments. 
+        Defaults to None in which case default values are used.
+    :returns: Output file path.
+    """
+    if model is None:
+        model = get_tts_model(TTS.list_models()[0])
+    output_path = "" if TEMPORARY_OUTPUT_PATH is None else output_path
+    synthesis_kwargs = {} if synthesis_kwargs is None else synthesis_kwargs
+    return model.tts_to_file(
+        text=text,
+        file_path=output_path,
+        **synthesis_kwargs)
