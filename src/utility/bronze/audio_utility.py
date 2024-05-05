f1c1d1d548bb73e3b0429de74aca3ce59127f559
Alexander Hering <dev.hering@gmail.com>
2024-05-05 19:10:57 +0200

    added function for getting audio devices

--- src/utility/bronze/audio_utility.py
+++ src/utility/bronze/audio_utility.py
@@ -5,3 +5,15 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
+import sounddevice
+import pyaudio
+
+
+def get_audio_devices(self, include_metadata: bool = False) -> list:
+    """
+    Returns a list of available devices.
+    :param include_metadata: Flag for including metadata.
+    :return: List of audio device names or audio device metadata dictionaries.
+    """
+    return [device if include_metadata else device["name"] for device in sounddevice.query_devices()]
+
