8b586249bfb7e1c558f4624235f4a81794871a1f
Alexander Hering <dev.hering@gmail.com>
2024-05-05 23:49:49 +0200

    added setter for audio devices

--- src/utility/gold/sound_generation/voice_assistant_abstractions.py
+++ src/utility/gold/sound_generation/voice_assistant_abstractions.py
@@ -57,3 +57,33 @@ class ConversationHandler(object):
         self.tts_processor = None
 
         self.interrupt = False
+
+    def set_input_device(self, input_device: Union[int, str] = None) -> None:
+        """
+        Sets input device for handler.
+        :param input_device: Name or index of input device.
+        """
+        if input_device is None:
+            pya = pyaudio.PyAudio()
+            self.input_device_index = pya.get_default_input_device_info().get("index")
+            pya.terminate()
+        else:
+            try:
+                self.input_device_index = input_device if isinstance(input_device, int) else [device for device in get_input_devices(include_metadata=True) if device["name"] == input_device][0]
+            except IndexError:
+                cfg.LOGGER.warning(f"Setting input device failed. Could not find device '{input_device}'.")
+
+    def set_output_device(self, output_device: Union[int, str] = None) -> None:
+        """
+        Sets output device for handler.
+        :param output_device: Name or index of output device.
+        """
+        if output_device is None:
+            pya = pyaudio.PyAudio()
+            self.output_device_index = pya.get_default_input_device_info().get("index")
+            pya.terminate()
+        else:
+            try:
+                self.input_device_index = output_device if isinstance(output_device, int) else [device for device in get_output_devices(include_metadata=True) if device["name"] == output_device][0]
+            except IndexError:
+                cfg.LOGGER.warning(f"Setting output device failed. Could not find device '{output_device}'.")
