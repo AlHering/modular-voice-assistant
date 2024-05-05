d5c122bdddfc48eb967001f90ef3393132944133
Alexander Hering <dev.hering@gmail.com>
2024-05-05 19:11:41 +0200

    added function for retrieving output devices

--- src/utility/bronze/audio_utility.py
+++ src/utility/bronze/audio_utility.py
@@ -25,3 +25,11 @@ def get_input_devices(self, include_metadata: bool = False) -> list:
     """
     return [device if include_metadata else device["name"] for device in sounddevice.query_devices() if device["max_input_channels"] > 0]
 
+def get_output_devices(self, include_metadata: bool = False) -> list:
+    """
+    Returns a list of available input devices.
+    :param include_metadata: Flag for including metadata.
+    :return: List of audio device names or audio device metadata dictionaries limited to output devices.
+    """
+    return [device if include_metadata else device["name"] for device in sounddevice.query_devices() if device["max_output_channels"] > 0]
+    
