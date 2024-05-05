8493757ec98198c4e77e0e8800db34f60cd34a52
Alexander Hering <dev.hering@gmail.com>
2024-05-05 23:23:46 +0200

    added function for faster whisper based transcription

--- src/utility/gold/sound_generation/speech_to_text_utility.py
+++ src/utility/gold/sound_generation/speech_to_text_utility.py
@@ -74,3 +74,33 @@ def get_faster_whisper_model(model_name_or_path: str, instantiation_kwargs: dict
         model_size_or_path=model_name_or_path,
         **instantiation_kwargs
     )
+
+
+def transcribe_with_whisper(audio_input: Union[str, np.ndarray, torch.Tensor] = None, model: whisper.Whisper = None, transcription_kwargs: dict = None) -> Tuple[str, List[dict]]:
+    """
+    Transcribes wave file or waveform with faster whisper.
+    :param audio_input: Wave file path or waveform.
+    :param output_path: Output path.
+        Defaults to None in which case the file "out.wav" under the temporary data folder is used.
+    :param model: Faster whisper model. 
+        Defaults to None in which case a default model is instantiated and used.
+        Not providing a model therefore increases processing time tremendously!
+    :param transcription_kwargs: Transcription keyword arguments. 
+        Defaults to None in which case default values are used.
+    :returns: Tuple of transcribed text and a list of metadata entries for the transcribed segments.
+    """
+    model = get_faster_whisper_model(model_name_or_path="large-v3") if model is None else model
+    audio_input = TEMPORARY_INPUT_PATH if audio_input is None else audio_input
+    transcription_kwargs = {} if transcription_kwargs is None else transcription_kwargs
+    
+    transcription, metadata = model.transcribe(
+        audio=audio_input,
+        **transcription_kwargs
+    )
+    metadata = metadata._asdict()
+    segment_metadatas = [segment._asdict() for segment in transcription]
+    for segment in segment_metadatas:
+        segment.update(metadata)
+    fulltext = " ".join([segment["text"].strip() for segment in segment_metadatas])
+    
+    return fulltext, segment_metadatas
