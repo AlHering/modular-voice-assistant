697c681a36e7b1ea46a4868d55e0010cf741708b
Alexander Hering <dev.hering@gmail.com>
2024-05-09 20:22:26 +0200

    refactored handler to use threads for llm and output

--- src/utility/gold/sound_generation/voice_assistant_abstractions.py
+++ src/utility/gold/sound_generation/voice_assistant_abstractions.py
@@ -13,9 +13,20 @@ import pyaudio
 import speech_recognition
 from datetime import datetime as dt
 from src.configuration import configuration as cfg
+from threading import Thread, Event as TEvent
+from queue import Empty, Queue as TQueue
 from ...bronze.audio_utility import get_input_devices, get_output_devices
 from ...bronze.time_utility import get_timestamp
 from . import speech_to_text_utility, text_to_speech_utility
+
+
+class InputMethod(Enum):
+    """
+    Represents input methods.
+    """
+    SPEECH_TO_TEXT = 0
+    COMMAND_LINE = 1
+    TEXT_FILE = 2
 
 
 class ConversationHandler(object):
@@ -33,7 +44,8 @@ class ConversationHandler(object):
                  tts_engine: str = None,
                  tts_model: str = None,
                  tts_instantiation_kwargs: dict = None,
-                 history: List[dict] = None) -> None:
+                 history: List[dict] = None,
+                 loop_pause: float = 0.1) -> None:
         """
         Initiation method.
         :param working_directory: Directory for productive files.
@@ -49,6 +61,8 @@ class ConversationHandler(object):
         :param tts_instantiation_kwargs: TTS model instantiation keyword arguments.
         :param history: History as list of dictionaries of the structure
             {"process": <"tts"/"stt">, "text": <text content>, "metadata": {...}}
+        :param loop_pause: Pause in seconds between processing loops.
+            Defaults to 0.1.
         """
         if not os.path.exists(working_directory):
             os.makedirs(working_directory)
@@ -61,10 +75,12 @@ class ConversationHandler(object):
         self.output_device_index = pya.get_default_output_device_info().get("index")
         pya.terminate()
 
-        self.stt_engine = None
-        self.tts_engine = None
-        self.stt_processor = None
-        self.tts_processor = None
+        self.stt_engine = stt_engine
+        self.stt_model = stt_model
+        self.stt_instantiation_kwargs = stt_instantiation_kwargs
+        self.tts_engine = tts_engine
+        self.tts_model = tts_model
+        self.tts_instantiation_kwargs = tts_instantiation_kwargs
 
         self.set_stt_processor(
             stt_engine=stt_engine,
@@ -77,10 +93,52 @@ class ConversationHandler(object):
             tts_instantiation_kwargs=tts_instantiation_kwargs
         )
 
-        self.history = [] if history is None else history
-        self.cache = {
-            "interrupt": False
-        }
+        self.input_queue = None
+        self.input_interrupt = None
+        self.llm_thread = None
+        self.llm_interrupt = None
+        self.output_thread = None
+        self.output_queue = None
+        self.output_interrupt = None
+
+        self.history = history
+        self.loop_pause = loop_pause
+        self.cache = None
+
+    def _reset(self, delete_history: bool = False) -> None:
+        """
+        Method for setting up and resetting handler. 
+        :param delete_history: Flag for declaring whether to delete history.    
+            Defaults to None.
+        """
+        self.set_stt_processor(
+            stt_engine=self.stt_engine,
+            stt_model=self.stt_model,
+            stt_instantiation_kwargs=self.stt_instantiation_kwargs
+        )
+        self.set_tts_processor(
+            tts_engine=self.tts_engine,
+            tts_model=self.tts_model,
+            tts_instantiation_kwargs=self.tts_instantiation_kwargs
+        )
+
+        self.input_queue = TQueue()
+        self.input_interrupt = TEvent()
+        self.llm_interrupt = TEvent()
+        self.llm_thread = Thread(
+            target=self.run_llm_process
+        )
+        self.output_queue = TQueue()
+        self.output_interrupt = TEvent()
+        self.output_thread = Thread(
+            target=self.run_tts_process
+        )
+        self.output_thread.daemon = True
+        self.output_thread.start()
+
+        self.history = [] if self.history is None or delete_history else self.history
+        self.cache = {}
+
 
     def set_input_device(self, input_device: Union[int, str] = None) -> None:
         """
@@ -200,26 +258,54 @@ class ConversationHandler(object):
         Acquires input based on text files.
         :return: Transcribed input and list of metadata entries.
         """
-        input_file = os.path.join(self.working_directory, "input.txt")
-        self.cache["text_input"] = self.cache.get("text_input", open(input_file, "r").readlines() if os.path.exists(input_file) else [])
         if self.cache["text_input"]:
             text = self.cache["text_input"][0]
             self.cache["text_input"] = self.cache["text_input"][1:]
             metadata_entries = [{"timestamp": get_timestamp(), "input_method": "text_file"}]
         return text, metadata_entries
 
-
-
-    def run_stt_process(self) -> None:
+    def run_stt_process(self, input_method: InputMethod = InputMethod.SPEECH_TO_TEXT) -> None:
         """
         Runs STT process.
+        :param inut_method: Input method out of SPEECH_TO_TEXT, COMMAND_LINE, TEXT_FILE.
+            Defaults to SPEECH_TO_TEXT.
+        """
+        if input_method == InputMethod.TEXT_FILE:
+            input_file = os.path.join(self.working_directory, "input.txt")
+            self.cache["text_input"] = self.cache.get("text_input", open(input_file, "r").readlines() if os.path.exists(input_file) else [])
+        input_handling = {
+            InputMethod.SPEECH_TO_TEXT: self.handle_stt_input,
+            InputMethod.COMMAND_LINE: self.handle_cli_input,
+            InputMethod.TEXT_FILE: self.handle_file_input
+        }[input_method]
+        while not self.input_interrupt.is_set():
+            new_input = input_handling()
+            if new_input[0]:
+                self.input_queue.put(new_input)
+
+    def run_llm_process(self) -> None:
+        """
+        Runs LLM process.
         """
         pass
-        
+
     def run_tts_process(self) -> None:
         """
         Runs TTS process.
-        :param tts_engine: TTS engine.
+        Should run in a separate thread to allow for continuous interaction.
         """
-        pass
-             
+        while not self.output_interrupt.is_set():
+            try:
+                output_text, output_metadatas = self.output_queue.get(self.loop_pause)
+                if output_text == "<EOS>":
+                    # Handle EOS stopping
+                    pass
+                elif output_text:
+                    # Handle output text
+                    pass
+                else:
+                    # Handle empty output text
+                    pass
+            except Empty:
+                # Handle empty queue stopping
+                pass
