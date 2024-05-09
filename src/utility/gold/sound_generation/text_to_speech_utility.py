a5bd593aeea20883d8fed74ec0a622febe68e925
Alexander Hering <dev.hering@gmail.com>
2024-05-09 21:11:12 +0200

    split playing file and data wave

--- src/utility/gold/sound_generation/text_to_speech_utility.py
+++ src/utility/gold/sound_generation/text_to_speech_utility.py
@@ -5,9 +5,10 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
-from typing import Any
+from typing import Any, Union, List
 import os
 import pyaudio
+import numpy as np
 import wave
 import torch
 from TTS.api import TTS
@@ -19,7 +20,7 @@ if not os.path.exists:
 TEMPORARY_OUTPUT_PATH = os.path.join(TEMPORARY_DATA_FOLDER, "out.wav")
 
 
-def play_wave(wave_file: str, chunk_size: int = 1024, stream_kwargs: dict = None) -> None:
+def play_wave_file(wave_file: str, chunk_size: int = 1024, stream_kwargs: dict = None) -> None:
     """
     Plays wave audio file.
     :param wave_file: Wave file path.
@@ -49,6 +50,32 @@ def play_wave(wave_file: str, chunk_size: int = 1024, stream_kwargs: dict = None
     pya.terminate()
 
 
+def play_wave(wave: Union[List[float], List[int], np.ndarray],
+              stream_kwargs: dict = None) -> None:
+    """
+    Plays wave audio file.
+    :param wave_file: Wave file path.
+    :param stream_kwargs: Stream keyword arguments.
+        Defaults to None in which case defaults are based on the wave file.
+    """
+    pya = pyaudio.PyAudio()
+    
+    stream_kwargs = {
+        "rate": 16000,
+        "format": pyaudio.paInt16,
+        "channels": 1
+    } if stream_kwargs is None else stream_kwargs
+    if "output" not in stream_kwargs:
+        stream_kwargs["output"] = True
+    stream = pya.open(
+        **stream_kwargs
+    )
+    stream.write(wave.tobytes())
+
+    stream.close()
+    pya.terminate()
+
+
 def get_coqui_tts_model(model_name_or_path: str, instantiation_kwargs: dict = None) -> TTS:
     """
     Returns a Coqui TTS based model instance.
@@ -70,9 +97,27 @@ def get_coqui_tts_model(model_name_or_path: str, instantiation_kwargs: dict = No
          )
 
 
-def synthesize_with_tts_to_file(text: str, output_path: str = None, model: TTS = None, synthesis_kwargs: dict = None) -> str:
+def synthesize_with_coqui_tts(text: str, model: TTS = None, synthesis_kwargs: dict = None) -> np.ndarray:
     """
-    Synthesizes text with TTS and saves results to a file.
+    Synthesizes text with Coqui TTS and saves results to a file.
+    :param text: Output text.
+    :param model: TTS model. 
+        Defaults to None in which case a default model is instantiated and used.
+        Not providing a model therefore increases processing time tremendously!
+    :param synthesis_kwargs: Synthesis keyword arguments. 
+        Defaults to None in which case default values are used.
+    :returns: Synthesized audio.
+    """
+    model = get_coqui_tts_model(TTS.list_models()[0]) if model is None else model
+    synthesis_kwargs = {} if synthesis_kwargs is None else synthesis_kwargs
+    return model.tts(
+        text=text,
+        **synthesis_kwargs)
+
+
+def synthesize_with_coqui_tts_to_file(text: str, output_path: str = None, model: TTS = None, synthesis_kwargs: dict = None) -> str:
+    """
+    Synthesizes text with Coqui TTS and saves results to a file.
     :param text: Output text.
     :param output_path: Output path.
         Defaults to None in which case the file "out.wav" under the temporary data folder is used.
