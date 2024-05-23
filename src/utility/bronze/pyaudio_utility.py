abd395fe8a3223e00d50dbace440674f9431d32e
Alexander Hering <dev.hering@gmail.com>
2024-05-23 12:16:44 +0200

    restructured audio utility

---
+++ src/utility/bronze/pyaudio_utility.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                     Utility                      *
+*            (c) 2024 Alexander Hering             *
+****************************************************
+"""
+import pyaudio
+import wave
+import numpy as np
+
+
+def play_wave_file(wave_file: str, chunk_size: int = 1024, stream_kwargs: dict = None) -> None:
+    """
+    Plays wave audio file.
+    :param wave_file: Wave file path.
+    :param chunk_size: Chunk size for file handling. 
+        Defaults to 1024.
+    :param stream_kwargs: Stream keyword arguments.
+        Defaults to None in which case defaults are based on the wave file.
+    """
+    pya = pyaudio.PyAudio()
+    output_file = wave.open(wave_file, "rb")
+    stream_kwargs = {
+        "rate": output_file.getnchannels(),
+        "format": pya.get_format_from_width(output_file.getsampwidth()),
+        "channels": output_file.getnchannels()
+    } if stream_kwargs is None else stream_kwargs
+    if "output" not in stream_kwargs:
+        stream_kwargs["output"] = True
+    stream = pya.open(
+        **stream_kwargs
+    )
+    data = output_file.readframes(chunk_size)
+    while data != "":
+        stream.write(data)
+        data = output_file.readframes(chunk_size)
+    stream.stop_stream()
+    stream.close()
+    pya.terminate()
+
+
+def play_wave(wave: np.ndarray,
+              stream_kwargs: dict = None) -> None:
+    """
+    Plays wave audio file.
+    :param wave: Waveform as numpy array.
+    :param stream_kwargs: Stream keyword arguments.
+        Defaults to None in which case defaults are used.
+    """
+    pya = pyaudio.PyAudio()
+    
+    stream_kwargs = {
+        "rate": 16000,
+        "format": pyaudio.paInt16,
+        "channels": 1
+    } if stream_kwargs is None else stream_kwargs
+    if "output" not in stream_kwargs:
+        stream_kwargs["output"] = True
+    stream = pya.open(
+        **stream_kwargs
+    )
+    stream.write(wave.tobytes())
+
+    stream.close()
+    pya.terminate()
