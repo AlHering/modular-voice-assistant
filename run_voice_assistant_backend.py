2471e8643b079406f9766ae8c00b0d00523272f8
Alexander Hering <dev.hering@gmail.com>
2024-06-04 20:21:55 +0200

    set uvicorn reload to false

--- run_voice_assistant_backend.py
+++ run_voice_assistant_backend.py
@@ -9,4 +9,4 @@ from src.interfaces import vocie_assistant_interface
 
 
 if __name__ == "__main__":
-    vocie_assistant_interface.run_backend()
+    vocie_assistant_interface.run_backend(reload=False)
