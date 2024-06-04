42b61d8e448d751b7696c675eb825aa17b2a02a0
Alexander Hering <dev.hering@gmail.com>
2024-06-04 20:23:01 +0200

    fixed up endpoints

--- src/interfaces/endpoints/voice_assistant_endpoints.py
+++ src/interfaces/endpoints/voice_assistant_endpoints.py
@@ -6,10 +6,12 @@
 ****************************************************
 """
 from typing import Callable, Optional, Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from pydantic import BaseModel
 import numpy as np
+import json
 from src.control.voice_assistant_controller import VoiceAssistantController
+from src.utility.bronze.pyaudio_utility import play_wave
 
 
 class Transcriber(BaseModel):
@@ -64,72 +66,79 @@ def register_endpoints(backend: FastAPI,
         "speech_recorder": SpeechRecorder
     }
 
-    for target in target_classes:
+    for target in list(target_classes.keys()):
         constructed_endpoint = endpoint_base + f"/{target}"
 
         @backend.get(f"{constructed_endpoint}")
         @interaction_decorator()
-        async def get_all() -> dict:
+        async def get_all(object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for getting all entries.
             :return: Response.
             """
-            return {f"{target}s": [controller.return_obj_as_dict(obj) for obj in controller.get_objects_by_type(target)]}
+            return {object_type: [controller.return_obj_as_dict(obj) for obj in controller.get_objects_by_type(object_type)]}
 
         @backend.post(f"{constructed_endpoint}")
         @interaction_decorator()
-        async def post(data: Union[Transcriber, Synthesizer, SpeechRecorder]) -> dict:
+        async def post(data: Union[Transcriber, Synthesizer, SpeechRecorder],
+                       object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for posting entries.
             :param data: Instance data.
             :return: Response.
             """
-            return {target: controller.return_obj_as_dict(controller.post_object(target, **dict(data)))}
+            return {object_type: controller.return_obj_as_dict(controller.post_object(object_type, **dict(data)))}
 
         @backend.get(f"{constructed_endpoint}/{{id}}")
         @interaction_decorator()
-        async def get(id: int) -> dict:
+        async def get(id: int,
+                      object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for getting entries.
             :param id: Instance ID.
             :return: Response.
             """
-            return {target: controller.return_obj_as_dict(controller.get_object_by_id(target, id))}
+            return {object_type: controller.return_obj_as_dict(controller.get_object_by_id(object_type, id))}
 
         @backend.delete(f"{constructed_endpoint}/{{id}}")
         @interaction_decorator()
-        async def delete(id: int) -> dict:
+        async def delete(id: int,
+                         object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for deleting entries.
             :param id: Instance ID.
             :return: Response.
             """
-            return {target: controller.return_obj_as_dict(controller.delete_object(target, id))}
+            return {object_type: controller.return_obj_as_dict(controller.delete_object(object_type, id))}
 
         @backend.patch(f"{constructed_endpoint}/{{id}}")
         @interaction_decorator()
-        async def patch(id: int, patch: dict) -> dict:
+        async def patch(id: int, patch: dict,
+                        object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for patching entries.
             :param id: Instance ID.
             :param patch: Patch payload.
             :return: Response.
             """
-            return {target: controller.return_obj_as_dict(controller.patch_object(target, id, **patch))}
+            return {object_type: controller.return_obj_as_dict(controller.patch_object(object_type, id, **patch))}
 
         @backend.put(f"{constructed_endpoint}")
         @interaction_decorator()
-        async def put(data: Union[Transcriber, Synthesizer, SpeechRecorder]) -> dict:
+        async def put(data: Union[Transcriber, Synthesizer, SpeechRecorder],
+                      object_type: str = Header(target, include_in_schema=False)) -> dict:
             """
             Endpoint for posting or updating entries.
             :param data: Instance data.
             :return: Response.
             """
-            return {target: controller.put_object(target, **dict(data))}
+            return {object_type: controller.put_object(object_type, **dict(data))}
         
-
-    @backend.get(f"{endpoint_base}/transcriber/{{id}}/transcribe")
-    async def transcribe(id: int, audio_input: list, transcription_parameters: dict = None) -> dict:
+    """
+    Extended interaction
+    """
+    @backend.post(f"{endpoint_base}/transcriber/{{id}}/transcribe")
+    async def transcribe(id: int, audio_input: list, transcription_parameters: Optional[dict] = None) -> dict:
         """
         Endpoint for transcribing.
         :param id: Transcriber ID.
@@ -138,13 +147,14 @@ def register_endpoints(backend: FastAPI,
             Defaults to None.
         :return: Response.
         """
+        transcription_parameters = transcription_parameters if transcription_parameters else None
         transcript, metadata = controller.transcribe(transcriber_id=id,
                                                      audio_input=np.ndarray(audio_input),
                                                      transcription_parameters=transcription_parameters)
         return {"transcript": transcript, "metadata": metadata}
     
-    @backend.get(f"{endpoint_base}/synthesizer/{{id}}/synthesize")
-    async def synthesize(id: int, text: str, synthesis_parameters: dict = None) -> dict:
+    @backend.post(f"{endpoint_base}/synthesizer/{{id}}/synthesize")
+    async def synthesize(id: int, text: str, synthesis_parameters: Optional[dict] = None) -> dict:
         """
         Endpoint for synthesis.
         :param id: Synthesizer ID.
@@ -153,13 +163,15 @@ def register_endpoints(backend: FastAPI,
             Defaults to None.
         :return: Response.
         """
-        synthesis, metadata = controller.synthesize(synthesizer_id=id,
-                                                    text=text,
-                                                    synthesis_parameters=synthesis_parameters)
-        return {"synthesis": synthesis.tolist(), "metadata": metadata}
+        synthesis_parameters = synthesis_parameters if synthesis_parameters else None
+        result = await controller.synthesize(synthesizer_id=id,
+                                       text=text,
+                                       synthesis_parameters=synthesis_parameters)
+        result[1]["dtype"] = str(result[0].dtype)
+        return {"synthesis": result[0].tolist(), "metadata": result[1]}
     
-    @backend.get(f"{endpoint_base}/speech_recorder/{{id}}/record")
-    async def record(id: int, recognizer_parameters: dict = None, microphone_parameters: dict = None) -> dict:
+    @backend.post(f"{endpoint_base}/speech_recorder/{{id}}/record")
+    async def record(id: int, recognizer_parameters: Optional[dict] = None, microphone_parameters: Optional[dict] = None) -> dict:
         """
         Endpoint for recording.
         :param id: SpeechRecorder ID.
@@ -169,6 +181,8 @@ def register_endpoints(backend: FastAPI,
             Defaults to None in which case default values are used.
         :return: Response.
         """
+        recognizer_parameters = recognizer_parameters if recognizer_parameters else None
+        microphone_parameters = microphone_parameters if microphone_parameters else None
         audio, metadata = controller.record(speech_recorder_id=id,
                                             recognizer_parameters=recognizer_parameters,
                                             microphone_parameters=microphone_parameters)
@@ -183,7 +197,9 @@ def register_endpoints(backend: FastAPI,
     }
         
     scheme = backend.openapi()
-    for path in scheme["paths"]:
+    for path in [path for path in scheme["paths"] 
+                 if not any(path.endswith(interaction_end) 
+                            for interaction_end in ["transcribe", "synthesize", "record"])]:
         target = path.replace(f"{endpoint_base}/", "").split("/")[0]
         if target in target_classes:
             for method in ["post", "put"]:
