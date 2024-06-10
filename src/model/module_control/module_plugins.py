03767dc3f2f6a613411e1e9e0f36205a9f112962
Alexander Hering <dev.hering@gmail.com>
2024-06-10 16:50:14 +0200

    starting text generation plugin structure

--- src/model/module_control/module_plugins.py
+++ src/model/module_control/module_plugins.py
@@ -5,3 +5,25 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
+from src.model.plugin_control.plugins import GenericPlugin, PluginImportException, PluginRuntimeException
+
+
+class TextGenerationPlugin(GenericPlugin):
+    """
+    Text Generation Plugin class.
+    """
+
+    def __init__(self, info: dict, path: str, security_hash: str = None, install_dependencies: bool = False) -> None:
+        """
+        Representation of a generic plugin.
+        :param info: Plugin info.
+        :param path: Path to plugin.
+        :param security_hash: Hash that can be used for authentication purposes.
+            Defaults to None.
+        :param install_dependencies: Flag, declaring whether to dynamically install dependencies. Defaults to False.
+            Only set this flag to True, if the code loaded through plugins is actively checked.
+        """
+        super().__init__(info=info, path=path, security_hash=security_hash, install_dependencies=install_dependencies)
+
+
+    
