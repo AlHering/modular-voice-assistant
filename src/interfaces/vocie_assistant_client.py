e302c2498d54f3749b4941788070366680018ddb
Alexander Hering <dev.hering@gmail.com>
2024-06-10 17:00:51 +0200

    starting client

---
+++ src/interfaces/vocie_assistant_client.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*             Modular Voice Assistant              *
+*            (c) 2024 Alexander Hering             *
+****************************************************
+"""
+import os
+from typing import Optional, Any, Union
+
+
+
+
