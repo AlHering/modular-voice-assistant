4f09254d7d1a312fc3c1cf25b445ffb840c28029
Alexander Hering <dev.hering@gmail.com>
2024-06-11 18:13:43 +0200

    added central VA components

--- src/model/voice_assistant_control/modules.py
+++ src/model/voice_assistant_control/modules.py
@@ -10,8 +10,8 @@ import traceback
 from abc import ABC, abstractmethod
 from queue import Queue
 import speech_recognition
-from src.utility.bronze import json_utility
-from src.utility.bronze import time_utility
+from src.utility.bronze import json_utility, time_utility, sounddevice_utility, pyaudio_utility
+from src.utility.gold.sound_generation.sound_model_abstractions import Transcriber, Synthesizer, SpeechRecorder
 from typing import List, Tuple, Any, Callable, Optional, Union, Dict, Generator
 from datetime import datetime as dt
 from enum import Enum
@@ -126,7 +126,7 @@ class FileInputModule(VoiceAssistantModule):
         
     def run(self) -> Tuple[str, dict]:
         """
-        Method for validating module functionality.
+        Method for running module.
         """
         self.index += 1
         return (self.lines[self.index], {
@@ -194,10 +194,147 @@ class PromptInputModule(VoiceAssistantModule):
         
     def run(self) -> Tuple[str, dict]:
         """
-        Method for validating module functionality.
+        Method for running module.
         """
         user_input = ""
         with patch_stdout():
             user_input = self.session.prompt(self.prompt_query)
         return user_input, {self.name: {"timestamp": time_utility.get_timestamp()}}
+    
+
+class SpeechRecognitionModule(VoiceAssistantModule):
+    """
+    SpeechRecognition module.
+    """
+
+    def __init__(self,
+                 recorder_input_device_index: int = None,
+                 recorder_recognizer_parameters: dict = None,
+                 recorder_microphone_parameters: dict = None,
+                 recorder_loop_pause = .1,
+                 transcriber_backend: str = None,
+                 transcriber_model_path: str = None,
+                 transcriber_model_parameters: dict = None,
+                 transcription_parameters: dict = None,
+                 skip_validation: bool = False) -> None:
+        """
+        Initiation method.
+        :param recorder_input_device_index: Input device index.
+            Defaults to None in which case the default input device index is fetched.
+        :param recorder_recognizer_parameters: Keyword arguments for setting up recognizer instances.
+            Defaults to None in which case default values are used.
+        :param recorder_microphone_parameters: Keyword arguments for setting up microphone instances.
+            Defaults to None in which case default values are used.
+        :param recorder_loop_pause: Forced pause between loops in seconds.
+            Defaults to 0.1.
+        :param transcriber_backend: Backend for transcriber model loading.
+            Check Transcriber.supported_backends for supported backends.
+        :param transcriber_model_path: Path to transcriber model files.
+            Defaults to None in which case a default model is used.
+            The latter will most likely result in it beeing downloaded.
+        :param transcriber_model_parameters: Transcriber model loading parameters as dictionary.
+            Defaults to None.
+        :param transcription_parameters: Transcription parameters as dictionary.
+            Defaults to None.
+        :param skip_validation: Flag for declaring whether or not to skip validation.
+            Defaults to False.
+        """
+        super().__init__(
+            name="SpeechRecognitionModule",
+            description="A module that records a single user speech input and transcribes it.",
+            skip_validation=skip_validation
+        )
+        self.speech_recorder = SpeechRecorder(
+            input_device_index=recorder_input_device_index,
+            recognizer_parameters=recorder_recognizer_parameters,
+            microphone_parameters=recorder_microphone_parameters,
+            loop_pause=recorder_loop_pause
+        )
+        self.transcriber = Transcriber(
+            backend=transcriber_backend,
+            model_path=transcriber_model_path,
+            model_parameters=transcriber_model_parameters,
+            transcription_parameters=transcription_parameters
+        )
+
+    def _validate(self) -> Tuple[bool, Dict]:
+        """
+        Method for validating module functionality.
+        """
+        available_indices = [
+            entry["index"] for entry in sounddevice_utility.get_input_devices(include_metadata=True)]
+        if self.speech_recorder.input_device_index in available_indices:
+            return False, {"reason": f"input device index not available ({available_indices})"}
+        return True, {"reason": "validation successful"}
+        
+    def run(self) -> Tuple[str, dict]:
+        """
+        Method for running module.
+        """
+        audio, recording_metadata = self.speech_recorder.record_single_input()
+        text, transcription_metadata = self.transcriber.transcribe(audio_input=audio)
+        return text, {self.name: {
+            "timestamp": time_utility.get_timestamp(),
+            "recording_metadata": recording_metadata,
+            "transcription_metadata": transcription_metadata
+        }} 
+
+
+class SynthesizedAudioOutputModule(VoiceAssistantModule):
+    """
+    Synthesized audio output module.
+    """
+
+    def __init__(self,
+                 synthesizer_backend: str,
+                 synthesizer_model_path: str = None,
+                 synthesizer_model_parameters: dict = None,
+                 synthesis_parameters: dict = None,
+                 skip_validation:bool = False) -> None:
+        """
+        Initiation method.
+        :param backend: Backend for model loading.
+            Check Synthesizer.supported_backends for supported backends.
+        :param model_path: Path to model files.
+            Defaults to None in which case a default model is used.
+            The latter will most likely result in it beeing downloaded.
+        :param model_parameters: Model loading parameters as dictionary.
+            Defaults to None.
+        :param synthesis_parameters: Synthesis parameters as dictionary.
+            Defaults to None.
+        :param skip_validation: Flag for declaring whether or not to skip validation.
+            Defaults to False.
+        """
+        super().__init__(
+            name="SynthesizedAudioOutputModule",
+            description="A module that outputs synthesized audio.",
+            skip_validation=skip_validation
+        )
+        self.synthesizer = Synthesizer(
+            backend=synthesizer_backend,
+            model_path=synthesizer_model_path,
+            model_parameters=synthesizer_model_parameters,
+            synthesis_parameters=synthesis_parameters
+        )
+
+    def _validate(self) -> Tuple[bool, Dict]:
+        """
+        Method for validating module functionality.
+        """
+        return True, {"reason": "validation successful"}
+        
+    def run(self, text: str, synthesis_parameters: dict = None) -> Tuple[np.ndarray, dict]:
+        """
+        Synthesize text to audio and outputs it.
+        :param text: Text to synthesize to audio.
+        :param synthesis_parameters: Synthesis parameters as dictionary.
+            Defaults to None.
+        :return: Audio data and metadata.
+        """
+        audio, synthesis_metadata = self.synthesizer.synthesize(text=text, synthesis_parameters=synthesis_parameters)
+        pyaudio_utility.play_wave(audio, synthesis_metadata)
+        return audio, {self.name: {
+            "timestamp": time_utility.get_timestamp(),
+            "synthesis_metadata": synthesis_metadata
+        }} 
     
