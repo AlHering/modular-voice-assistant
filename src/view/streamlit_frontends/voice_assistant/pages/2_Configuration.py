24a1295460cf187a48ed4cec7660c12b1ad1f4c7
Alexander Hering <dev.hering@gmail.com>
2024-06-12 20:24:47 +0200

    finished up separation, parameterization and speech recorder config

--- src/view/streamlit_frontends/voice_assistant/pages/2_Configuration.py
+++ src/view/streamlit_frontends/voice_assistant/pages/2_Configuration.py
@@ -18,9 +18,6 @@ from src.view.streamlit_frontends.voice_assistant.utility.frontend_rendering imp
 ###################
 # Main page functionality
 ###################
-TRANSCRIBER_TAB_KEY = "new_transcriber"
-SYNTHESIZER_TAB_KEY = "new_synthesizer"
-SPEECH_RECORDER_TAB_KEY = "new_speech_recorder"
 
 
 def get_json_parameters(object_type: str) -> List[str]:
@@ -44,12 +41,10 @@ def gather_config(object_type: str) -> dict:
     :return: Object config.
     """
     if object_type in ["transcriber", "synthesizer"]: 
-        data = {
-            key: st.session_state[f"new_{object_type}_{key}"] for key in [
-                "backend", "model_path"]
-        }
+        params = ["backend", "model_path"]
     elif object_type == "speech_recorder":
-        pass
+        params = ["input_device_index", "loop_pause"]
+    data = {key: st.session_state[f"new_{object_type}_{key}"] for key in params}
     for key in get_json_parameters(object_type):
         widget = st.session_state[f"new_{object_type}_{key}"]
         data[key] = json.loads(widget["text"]) if widget is not None else None
@@ -63,8 +58,8 @@ def render_config(object_type: str) -> None:
     """
     tab_key = f"new_{object_type}"
     available = {entry.id: entry for entry in st.session_state["CONTROLLER"].get_objects_by_type(object_type)}
-    options = [">>New<<"] + list(available.keys())
-    default = st.session_state.get(f"{tab_key}_overwrite_config_id", st.session_state.get(f"{object_type}_config_selectbox", ">>New<<"))
+    options = [">> New <<"] + list(available.keys())
+    default = st.session_state.get(f"{tab_key}_overwrite_config_id", st.session_state.get(f"{object_type}_config_selectbox", ">> New <<"))
     
     header_columns = st.columns([.25, .10, .65])
     header_button_columns = header_columns[2].columns([.30, .30, .30])
@@ -79,10 +74,11 @@ def render_config(object_type: str) -> None:
     )
     
     current_config = available.get(st.session_state[f"{object_type}_config_selectbox"])
-    backends = st.session_state["CLASSES"][object_type].supported_backends if hasattr(st.session_state["CLASSES"][object_type], "supported_backends") else None
-    default_models = st.session_state["CLASSES"][object_type].default_models if hasattr(st.session_state["CLASSES"][object_type], "default_models") else None
-
+    
     if object_type in ["transcriber", "synthesizer"]:
+        backends = st.session_state["CLASSES"][object_type].supported_backends
+        default_models = st.session_state["CLASSES"][object_type].default_models
+
         st.selectbox(
             key=f"{tab_key}_backend", 
             label="Backend", 
@@ -95,7 +91,40 @@ def render_config(object_type: str) -> None:
             key=f"{tab_key}_model_path", 
             label="Model")
     elif object_type == "speech_recorder":
-        pass
+        input_devices = {entry["name"]: entry["index"] 
+                         for entry in sorted(
+                             st.session_state["CLASSES"][object_type].supported_input_devices,
+                             key=lambda x: x["index"])}
+        input_device_column, loop_pause_column, _ = st.columns([.25, .25, .50])
+        current_device_index = 0 
+        if current_config is not None:
+            for device_name in input_devices:
+                if current_config.input_device_index == input_devices[device_name]:
+                    current_device_index = input_devices[device_name]
+                    break
+        device_name = input_device_column.selectbox(
+            key=f"{tab_key}_input_device_name", 
+            label="Input device", 
+            options=list(input_devices.keys()), 
+            index=current_device_index)
+        st.session_state[f"{tab_key}_input_device_index"] = input_devices[device_name]
+        st.markdown("""
+        <style>
+            button.step-up {display: none;}
+            button.step-down {display: none;}
+            div[data-baseweb] {border-radius: 4px;}
+        </style>""",
+        unsafe_allow_html=True)
+        loop_pause_column.number_input(
+            "Loop pause",
+            key=f"{tab_key}_loop_pause", 
+            format="%0.2f",
+            step=0.1,
+            min_value=0.01,
+            max_value=10.1,
+            value=.1 if current_config is None else current_config.loop_pause
+        )
+
 
     st.write("")
     for parameter in get_json_parameters(object_type):
@@ -106,11 +135,12 @@ def render_config(object_type: str) -> None:
 
     header_button_columns[0].write("#####")
     object_title = " ".join(object_type.split("_")).title()
-    with header_button_columns[0].popover("Overwrite", disabled=current_config is None, 
-                                help="Overwrite the current configuration"):
+    with header_button_columns[0].popover("Overwrite",
+                                          disabled=current_config is None, 
+                                          help="Overwrite the current configuration"):
             st.write(f"{object_title} configuration {st.session_state[f'{object_type}_config_selectbox']} will be overwritten.")
             
-            if st.button("Approve"):
+            if st.button("Approve", key=f"{tab_key}_approve_btn",):
                 obj_id = st.session_state["CONTROLLER"].patch_object(
                     object_type,
                     st.session_state[f"{object_type}_config_selectbox"],
@@ -119,7 +149,9 @@ def render_config(object_type: str) -> None:
                 st.info(f"Updated {object_title} configuration {obj_id}.")
 
     header_button_columns[1].write("#####")
-    if header_button_columns[1].button("Add new", help="Add new entry with the below configuration if it does not exist yet."):
+    if header_button_columns[1].button("Add new", 
+                                       key=f"{tab_key}_add_btn",
+                                       help="Add new entry with the below configuration if it does not exist yet."):
         obj_id = st.session_state["CONTROLLER"].put_object(
             object_type,
             **gather_config(object_type)
@@ -152,16 +184,9 @@ if __name__ == "__main__":
     # Page content
     st.title("Configuration")
     
-    transcriber_tab, synthesizer_tab, recorder_tab = st.tabs(["Transcribers", "Synthesizers", "Speech Recorders"])
-
-    with transcriber_tab:
-        render_config("transcriber")
-
-    with synthesizer_tab:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-    with recorder_tab:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    tabs = ["transcriber", "synthesizer", "speech_recorder"]
+    for index, tab in enumerate(st.tabs([" ".join(elem.split("_")).title()+"s" for elem in tabs])):
+        with tab:
+            render_config(tabs[index])
+            
     render_sidebar()
