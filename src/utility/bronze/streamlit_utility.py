083884bb9306015b7d1d657cfcdca636e8c48ac5
Alexander Hering <dev.hering@gmail.com>
2024-06-15 17:01:18 +0200

    added json input rendering to core utility

--- src/utility/bronze/streamlit_utility.py
+++ src/utility/bronze/streamlit_utility.py
@@ -5,9 +5,14 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
-from typing import Any
+from typing import Any, List
 from time import sleep
 import streamlit as st
+import json
+try:
+    from code_editor import code_editor
+except ImportError:
+    pass
 from datetime import datetime as dt
 
 
@@ -55,3 +60,79 @@ def update_bound_state_dictionary(field: str, update: dict) -> None:
         st.session_state[field].drop(
             st.session_state[field].index[entry_index], inplace=True)
     update["deleted_rows"] = []
+
+
+def get_json_editor_buttons() -> List[dict]:
+    """
+    Function for acquiring json payload code editor buttons.
+    Commands can be found at https://github.com/ajaxorg/ace/blob/v1.2.6/lib/ace/commands/default_commands.js.
+    :return: Buttons as list of dictionaries.
+    """
+    return [
+        {
+            "name": "save",
+            "feather": "Save",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": [
+                    "save-state",
+                    [
+                        "response",
+                        "saved"
+                    ]
+            ],
+            "response": "saved",
+            "style": {"top": "0rem", "right": "9.6rem"}
+        },
+        {
+            "name": "copy",
+            "feather": "Copy",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": ["copyAll"],
+            "style": {"top": "0rem", "right": "5rem"}
+        },
+        {
+            "name": "clear",
+            "feather": "X",
+            "hasText": True,
+            "alwaysOn": True,
+            "commands": ["selectall", "del", ["insertstring", "{\n\t\n}"], "save-state",
+                         ["response", "saved"]],
+            "style": {"top": "0rem", "right": "0.4rem"}
+        },
+    ]
+
+
+def render_json_input(parent_widget: Any, key: str, label: str = None, default_data: dict = None) -> None:
+    """
+    Function for rendering JSON input.
+    :param parent_widget: Parent widget.
+    :param key: Widget key.
+    :param label: Optional label.
+    :param default_data: Default data.
+    """
+    if label is not None:
+        parent_widget.write(label)
+    with parent_widget.empty():
+        widget = content = st.session_state["CACHE"].get(key)
+        if widget is not None:
+            content = widget["text"]
+        else:
+            content = json.dumps(
+                default_data, 
+                indent=4, 
+                ensure_ascii=False
+            )
+        content = "{\n\t\n}" if content == "{}" else content
+        code_editor(
+            content,
+            key=key,
+            lang="json",
+            allow_reset=True,
+            options={"wrap": True},
+            buttons=get_json_editor_buttons(),
+            response_mode="debounce"
+        )
+
+        
