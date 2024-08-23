2f673e7c7c4641d0a55a71e7634ea09b0632808f
Alexander Hering <dev.hering@gmail.com>
2024-08-23 20:35:19 +0200

    added docs

--- src/utility/gold/text_generation/persona_abstractions.py
+++ src/utility/gold/text_generation/persona_abstractions.py
@@ -5,7 +5,7 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
-from typing import List, Any
+from typing import List, Any, Union
 from pydantic import BaseModel
 from .language_model_abstractions import ChatModelInstance, RemoteChatModelInstance
 from .knowledgebase_abstractions import MemoryMetadata, MemoryEntry, Memory
@@ -26,16 +26,27 @@ class Persona(object):
     """
     def __init__(self, 
                  persona_description: str,
+                 chat_model_instance: Union[ChatModelInstance, RemoteChatModelInstance],
                  conversation_example: str | None = None,
                  memories: List[MemoryEntry] | None = None) -> None:
+        """
+        Initiation method.
+        :param persona_description: Persona character description.
+        :param chat_model_instance: Chat model instance.
+        :param conversation_example: An conversation example.
+        :param memories: List of memories to initate the Persona with.
+        :param remote: Flag that declares, whether to use an 
+        """
         self.persona_description = persona_description
         self.conversation_example = conversation_example
         self.memories = memories
 
+
+
     @classmethod
     def from_configuration(cls, config: PersonaConfiguration) -> Any:
         """
-        Returns Persona instance from configuration.
+        Returns a persona instance from configuration.
         :param config: Persona configuration.
         :return: Persona instance.
         """
