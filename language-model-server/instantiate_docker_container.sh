8ad749feeec820302bc499c032d012cf242e4e4d
Alexander Hering <dev.hering@gmail.com>
2024-10-31 14:11:25 +0100

    adjusted requirements and versioning

--- language-model-server/instantiate_docker_container.sh
+++ language-model-server/instantiate_docker_container.sh
@@ -3,4 +3,4 @@ docker run  \
     -it --net=host --gpus all \
     --mount type=bind,source="$1",target=/llama-cpp-server/models \
     --mount type=bind,source="$2",target=/llama-cpp-server/configs \
-    "llama-cpp-server:v0.1" /bin/bash
+    "llama-cpp-server:v0.2" /bin/bash
