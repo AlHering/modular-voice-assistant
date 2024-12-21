44b79b73c78d7298f42cca80233f60fe85ee631d
Alexander Hering <dev.hering@gmail.com>
2024-12-21 15:01:48 +0100

    removed unnecessary init from inheriting class

--- src/backend/voice_assistant/modular_voice_assistant_abstractions.py
+++ src/backend/voice_assistant/modular_voice_assistant_abstractions.py
@@ -264,16 +264,6 @@ class WaveOutputModule(VAModule):
     Takes in VAPackages with wave audio data and outputs it. 
     Note, that the last metadata stack element must be stream parameters for outputting.
     """
-    def __init__(self, 
-                 *args: Any | None, 
-                 **kwargs: Any | None) -> None:
-        """
-        Initiates an instance.
-        :param args: Arbitrary arguments.
-        :param kwargs: Arbitrary keyword arguments.
-        """
-        super().__init__(*args, **kwargs)
-
     def process(self) -> VAPackage | Generator[VAPackage, None, None] | None:
         """
         Module processing method.
