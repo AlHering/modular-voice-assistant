afb9c62d95acc24e64b0a591ca5ce8d4b53bf07f
Alexander Hering <dev.hering@gmail.com>
2024-12-22 15:11:48 +0100

    added default entries

--- src/backend/database/data_model.py
+++ src/backend/database/data_model.py
@@ -5,10 +5,12 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
+import os
 from sqlalchemy.orm import relationship, mapped_column, declarative_base
 from sqlalchemy import Engine, Column, String, JSON, ForeignKey, Integer, DateTime, func, Uuid, Text, event, Boolean
 from uuid import uuid4, UUID
 from typing import Any
+from src.configuration import configuration as cfg
 
 
 def populate_data_infrastructure(engine: Engine, schema: str, model: dict) -> None:
@@ -67,3 +69,65 @@ def populate_data_infrastructure(engine: Engine, schema: str, model: dict) -> No
         model[dataclass.__tablename__.replace(schema, "")] = dataclass
 
     base.metadata.create_all(bind=engine)
+
+
+def get_default_entries() -> dict:
+    """
+    Returns default entries.
+    :return: Default entries.
+    """
+    transcriber_config = {
+        "id": 1,
+        "config_type": "transcriber",
+        "config": {
+            "backend": "faster-whisper",
+            "model_path": os.path.join(cfg.PATHS.MODEL_PATH, 
+                                    "sound_generation_models/speech_to_text/faster_whisper_models/Systran_faster-whisper-tiny"),
+            "model_parameters": {
+                "device": "cuda",
+                "compute_type": "float32",
+                "local_files_only": True
+            }
+        }
+    }
+
+    fallback_synthesizer_model = os.path.join(cfg.PATHS.MODEL_PATH, 
+                                              "sound_generation_models/text_to_speech/coqui_models/tts_models-multilingual-multi-dataset-xtts_v2")
+    fallback_speaker_wav = os.path.join(cfg.PATHS.MODEL_PATH, "sound_generation_models//text_to_speech/coqui_xtts/examples/female.wav")
+    synthesizer_config = {
+        "id": 2,
+        "config_type": "synthesizer",
+        "config": {
+            "backend": "coqui-tts",
+            "model_path": fallback_synthesizer_model,
+            "model_parameters": {
+                "config_path": f"{fallback_synthesizer_model}/config.json",
+                "gpu": True
+            },
+            "synthesis_parameters": {
+                "speaker_wav": fallback_speaker_wav,
+                "language": "en"
+            }
+        }
+    }
+
+    chat_model_config = {
+        "id": 3,
+        "config_type": "chat_model",
+        "config": {
+            "model_path": os.path.join(cfg.PATHS.MODEL_PATH, 
+                                        "text_generation_models/mradermacher_Llama-3.1-Storm-8B-i1-GGUF"),
+            "model_file": "Llama-3.1-Storm-8B.i1-Q4_K_M.gguf",
+            "model_parameters": {
+                "n_ctx": 4096, 
+                "temperature": 0.8, 
+                "repetition_penalty": 1.6,
+                "n_gpu_layers": 33
+            },
+            "generating_parameters": {
+                "max_tokens": 256
+            }
+        }
+    }
+
+    return {"Config": [transcriber_config, synthesizer_config, chat_model_config]}
