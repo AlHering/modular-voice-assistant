0b67c677a1b949cebcce110f22524064f103c354
Alexander Hering <dev.hering@gmail.com>
2024-12-26 22:50:40 +0100

    added loading for common components

--- src/frontend/streamlit/utility/backend_interaction.py
+++ src/frontend/streamlit/utility/backend_interaction.py
@@ -18,7 +18,7 @@ from httpx import RequestError, ConnectError, ConnectTimeout
 from http.client import responses as status_codes
 from src.backend.database.basic_sqlalchemy_interface import BasicSQLAlchemyInterface, FilterMask
 from src.backend.database.data_model import populate_data_infrastructure, get_default_entries
-from src.backend.voice_assistant.modular_voice_assistant_abstractions_v2 import BasicVoiceAssistant, Transcriber, Synthesizer, SpeechRecorder, BaseModuleSet, BasicHandlerModule, WaveOutputModule, SpeechRecorderModule, ModularConversationHandler
+from src.backend.voice_assistant.modular_voice_assistant_abstractions_v2 import BasicVoiceAssistant, ChatModelInstance, RemoteChatModelInstance, Transcriber, Synthesizer, SpeechRecorder, BaseModuleSet, BasicHandlerModule, WaveOutputModule, SpeechRecorderModule, ModularConversationHandler
 from run_backend import setup_default_voice_assistant
 from src.configuration import configuration as cfg
 
@@ -67,11 +67,12 @@ def setup() -> None:
     }
 
 
-def load_assistant(module_set: BaseModuleSet, loop_pause: float = .1) -> BasicVoiceAssistant:
+def load_conversation_handler(module_set: BaseModuleSet, loop_pause: float = .1) -> ModularConversationHandler:
     """
     Loads a basic voice assistant.
     :param module_set: Module set.
     :param loop_pause: Loop pause for modules.
+    :return: Conversation handler.
     """
     return ModularConversationHandler(working_directory=os.path.join(st.session_state["WORKDIR"], "conversation_handler"),
                                       module_set=module_set,
@@ -81,11 +82,27 @@ def load_assistant(module_set: BaseModuleSet, loop_pause: float = .1) -> BasicVo
 def get_components() -> List[Dict[str, dict]]:
     """
     Retrieves available components.
-    :returns: List of components.
+    :returns: Dictionary of components.
     """
     return {
         "transcriber": st.session_state["DATABASE"].get_objects_by_type(object_type="transcriber"),
         "synthesizer": st.session_state["DATABASE"].get_objects_by_type(object_type="synthesizer"),
         "speech_recorder": st.session_state["DATABASE"].get_objects_by_type(object_type="speech_recorder"),
         "chat_model": st.session_state["DATABASE"].get_objects_by_type(object_type="chat_model"),
+        "remote_chat_model": st.session_state["DATABASE"].get_objects_by_type(object_type="remote_chat_model"),
     }
+
+
+def load_component(object_type: str, object_config: dict) -> Transcriber | Synthesizer | SpeechRecorder | ChatModelInstance | RemoteChatModelInstance:
+    """
+    Loads a component.
+    :param object_type: Object type.
+    :param object_config: Object config. 
+    """
+    return {
+        "transcriber": Transcriber,
+        "synthesizer": Synthesizer,
+        "speech_recorder": SpeechRecorder,
+        "chat_model": ChatModelInstance,
+        "remote_chat_model": RemoteChatModelInstance,
+    }[object_type](**object_config)
