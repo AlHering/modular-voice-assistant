41d85013d36a1813ece4d8d464d7f7542d7fb825
Alexander Hering <dev.hering@gmail.com>
2024-12-28 15:28:32 +0100

    implementing worker modules

--- src/modules/worker_modules.py
+++ src/modules/worker_modules.py
@@ -5,20 +5,58 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
-from typing import Any, Tuple, List, Callable, Generator
-import numpy as np
-from queue import Empty
-from pydantic import BaseModel, ConfigDict
-from src.utility.commandline_utility import silence_stderr
-from src.utility.pyaudio_utility import play_wave
-from src.utility.language_model_abstractions import ChatModelConfig, ChatModelInstance, RemoteChatModelConfig, RemoteChatModelInstance
-from src.utility.sound_model_abstractions import Transcriber, Synthesizer, SpeechRecorder
-from src.modules.abstractions import VAModule, VAPackage, VAModuleConfig, BasicHandlerModule
+from typing import Any, List, Callable, Dict
+from src.utility.language_model_abstractions import LanguageModelConfig, ChatModelInstance, RemoteChatModelInstance
+from src.modules.abstractions import BasicHandlerModule
 
 
-class ChatModule(BasicHandlerModule):
+class LocalChatModule(BasicHandlerModule):
     """
-    Chat module.
+    Local chat module.
+    Generates a response for a given input.
+    """
+    def __init__(self, 
+                 model_path: str,
+                 model_file: str | None = None,
+                 model_parameters: dict | None = None,
+                 encoding_parameters: dict | None = None,
+                 embedding_parameters: dict | None = None,
+                 generating_parameters: dict | None = None,
+                 decoding_parameters: dict | None = None,
+                 chat_parameters: dict | None = None,
+                 system_prompt: str | None = None,
+                 prompt_maker: Callable | str | None = None,
+                 use_history: bool = True,
+                 history: List[Dict[str, str | dict]] | None = None) -> None:
+        """
+        Initiates an instance.
+        :param backend: Backend for model loading.
+            Check Transcriber.supported_backends for supported backends.
+        :param model_path: Path to model files.
+            Defaults to None in which case a default model is used.
+            The latter will most likely result in it being downloaded.
+        :param model_parameters: Model loading parameters as dictionary.
+            Defaults to None.
+        :param synthesis_parameters: Synthesis parameters as dictionary.
+            Defaults to None.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        """
+        self.language_model = LanguageModelConfig(
+
+        )
+        self.chat_model = ChatModelInstance(
+            backend=backend,
+            model_path=model_path,
+            model_parameters=model_parameters,
+            synthesis_parameters=synthesis_parameters
+        )
+        super().__init__(handler_method=self.synthesizer.synthesize, *args, **kwargs)
+
+
+class RemoteChatModule(BasicHandlerModule):
+    """
+    Remote chat module.
     Generates a response for a given input.
     """
     def __init__(self, 
