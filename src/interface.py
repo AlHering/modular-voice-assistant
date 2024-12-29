b5480a2c0f87a42e0eedb1d20ed86a6b597a00e9
Alexander Hering <dev.hering@gmail.com>
2024-12-29 01:11:25 +0100

    building out interface

--- src/interface.py
+++ src/interface.py
@@ -6,13 +6,17 @@
 ****************************************************
 """
 import os
-from typing import List
+from typing import List, Generator
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 import numpy as np
+from uuid import UUID
+from gc import collect
 from src.configuration import configuration as cfg
 from src.database.basic_sqlalchemy_interface import BasicSQLAlchemyInterface, FilterMask
 from src.database.data_model import populate_data_infrastructure, get_default_entries
-from src.voice_assistant import AVAILABLE_MODULES, SpeechRecorderModule, TranscriberModule, SynthesizerModule, LocalChatModule, Re
+from src.voice_assistant import AVAILABLE_MODULES, BasicVoiceAssistant, BasicHandlerModule, SpeechRecorderModule, TranscriberModule, SynthesizerModule, LocalChatModule, WaveOutputModule, RemoteChatModule
+from src.modules.abstractions import VAModule, VAPackage
 
 
 class VoiceAssistantInterface(object):
@@ -28,15 +32,15 @@ class VoiceAssistantInterface(object):
         self.database = BasicSQLAlchemyInterface(
             working_directory=os.path.join(self.working_directory, "database"),
             population_function=populate_data_infrastructure,
-            default_entries=get_default_entries()
+            default_entries=get_default_entries(),
+            handle_objects_as_dicts=True,
+            convert_timestamps=True,
+            convert_uuids=True
         )
         
-        self.current_assistant_config = {
-            "speech_recorder": None,
-            "transcriber": None,
-            "synthesizer": None,
-            "chat_model": None,
-        }
+        self.modules = {key: None for key in AVAILABLE_MODULES}
+        self.module_uuids = {key: None for key in AVAILABLE_MODULES}
+        self.module_titles = {key: " ".join(key.split("_")).title() for key in AVAILABLE_MODULES}
         self.assistant: BasicVoiceAssistant | None = None
         self.router: APIRouter | None = None
 
@@ -55,97 +59,167 @@ class VoiceAssistantInterface(object):
         self.router.add_api_route(path="/assistant/transcribe", endpoint=self.transcribe, methods=["POST"])
         self.router.add_api_route(path="/assistant/synthesize", endpoint=self.synthesize, methods=["POST"])
 
+    """
+    Config handling
+    """
+
     def add_config(self,
                    module_type: str,
-                   config: dict,
-                   validate: bool = False) -> dict:
+                   config: dict) -> dict:
         """
         Adds a config to the database.
         :param module_type: Target module type.
         :param config: Config.
-        :param validate: Flag for triggering validation.
         :return: Response.
         """
-
-    def setup_assistant(self, 
-                        speech_recorder_id: int| None = None,
-                        transcriber_id: int| None = None,
-                        synthesizer_id: int| None = None,
-                        chat_model_id: int| None = None) -> dict:
+        return self.database.put_object(object_type="module_config", module_type=module_type, **config)
+    
+    def get_configs(self,
+                    module_type: str = None) -> List[dict]:
         """
-        Sets up a voice assistant.
-        :param speech_recorder_id: Config ID for the speech recorder.
-        :param transcriber_id: Config ID for the transcriber.
-        :param synthesizer_id: Config ID for the synthesizer.
-        :param chat_model_id: Config ID for the chat model.
+        Adds a config to the database.
+        :param module_type: Target module type.
+            Defaults to None in which case all configs are returned.
+        :return: Response.
         """
-        speech_recorder_configs = self.database.get_objects_by_filtermasks(
-                object_type="config", 
-                filtermasks=FilterMask(expressions=[["config_type", "==", "speech_recorder"]] if speech_recorder_id is None else 
-                        [["config_type", "==", "speech_recorder", ["id", "==", speech_recorder_id]]])
-            )
-        transcriber_configs = self.database.get_objects_by_filtermasks(
-            object_type="config", 
-            filtermasks=FilterMask(expressions=[["config_type", "==", "transcriber"]] if transcriber_id is None else 
-                    [["config_type", "==", "transcriber", ["id", "==", transcriber_id]]])
-        )
-        synthesizer_configs = self.database.get_objects_by_filtermasks(
-            object_type="config", 
-            filtermasks=FilterMask(expressions=[["config_type", "==", "synthesizer"]] if synthesizer_id is None else 
-                    [["config_type", "==", "synthesizer", ["id", "==", synthesizer_id]]])
-        )
-        chat_model_configs = self.database.get_objects_by_filtermasks(
-            object_type="config", 
-            filtermasks=FilterMask(expressions=[["config_type", "==", "chat_model"]] if chat_model_id is None else 
-                    [["config_type", "==", "chat_model", ["id", "==", chat_model_id]]])
-        )
-
-        speech_recorder_config = speech_recorder_configs[0]
-        transcriber_config = transcriber_configs[0]
-        synthesizer_config = synthesizer_configs[0]
-        chat_model_config = chat_model_configs[0]
-        
-        if self.assistant is None:
-            self.assistant = BasicVoiceAssistant(
-                working_directory=os.path.join(self.working_directory, "voice_assistant"),
-                speech_recorder=SpeechRecorder(**speech_recorder_config.config),
-                transcriber=Transcriber(**transcriber_config.config),
-                synthesizer=Synthesizer(**synthesizer_config.config),
-                chat_model=ChatModelInstance(**chat_model_config.config) if chat_model_config.mode == "local"
-                    else RemoteChatModelInstance(**chat_model_config.config)
-            )
+        if module_type is None:
+            return self.database.get_objects_by_type(object_type="module_config")
         else:
-            if speech_recorder_id and self.current_assistant_config["speech_recorder"] != speech_recorder_id:
-                self.assistant.speech_recorder = SpeechRecorder(**speech_recorder_config.config)
-            if transcriber_id and self.current_assistant_config["transcriber"] != transcriber_id:
-                self.assistant.speech_recorder = Transcriber(**speech_recorder_config.config)
-            if synthesizer_id and self.current_assistant_config["synthesizer"] != synthesizer_id:
-                self.assistant.synthesizer = Synthesizer(**synthesizer_config.config)
-            if chat_model_id and self.current_assistant_config["chat_model"] != chat_model_id:
-                self.assistant.chat_model = SpeechRecorder(**chat_model_config.config)
+            return self.database.get_objects_by_filtermasks(object_type="module_config", filtermasks=[FilterMask([["module_type", "==", module_type]])])
+        
+    """
+    Module handling
+    """
+
+    def load_module(self,
+                    module_type: str,
+                    config_uuid: str) -> dict:
+        """
+        Loads a module from the given config UUID.
+        :param module_type: Target module type.
+        :param config_uuid: Config UUID.
+        :return: Response.
+        """
+        config_uuid = UUID(config_uuid)
+        if self.module_uuids[module_type] != config_uuid:
+            self.unload_module(module_type=module_type, config_uuid=self.module_uuids[module_type])
+            entry = self.database.get_object_by_id("module_config", object_id=config_uuid)
+            if entry:
+                self.modules[module_type] = AVAILABLE_MODULES[module_type](**entry["config"])
+                self.module_uuids[module_type] = UUID(entry["id"])
+                return {"success": f"Set active {self.module_titles[module_type]} to UUID '{config_uuid}':\n{entry}"}
+            else:
+                return {"error": f"No {self.module_titles[module_type]} with UUID '{config_uuid}'"}
+        else:
+            return {"success": f"Active {self.module_titles[module_type]} is already set to UUID '{config_uuid}'"}
             
-        self.current_assistant_config = {
-            "speech_recorder": speech_recorder_config.id,
-            "transcriber": transcriber_config.id,
-            "synthesizer": synthesizer_config.id,
-            "chat_model": chat_model_config.id,
-        }
+    def unload_module(self,
+                      module_type: str,
+                      config_uuid: str) -> dict:
+        """
+        Unloads a module from the given config UUID.
+        :param module_type: Target module type.
+        :param config_uuid: Config UUID.
+        :return: Response.
+        """
+        config_uuid = UUID(config_uuid)
+        if self.module_uuids[module_type] != config_uuid:
+            return {"error": f"Active {self.module_titles[module_type]} has UUID '{self.module_uuids[module_type]}', not '{config_uuid}'"}
+        elif self.module_uuids[module_type] is not None:
+            module_obj = self.modules.pop(module_type) 
+            del module_obj
+            self.module_uuids[module_type] = None
+            collect()
+            return {"success": f"Unloaded {self.module_titles[module_type]} with config '{config_uuid}'"}
+        else:
+            return {"error": f"No active {self.module_titles[module_type]}"}
+
+    """
+    Assistant handling
+    """
+
+    def setup_assistant(self,
+                        speech_recorder_uuid: str,
+                        transcriber_uuid: str,
+                        worker_uuid: str,
+                        synthesizer_uuid: str,
+                        wave_output_uuid: str,
+                        stream: bool = True,
+                        forward_logging: bool = False,
+                        report: bool = False) -> dict:
+        """
+        Sets up a voice assistant from currently configured modules.
+        :param speech_recorder_uuid: Speech Recorder config UUID.
+        :param transcriber_uuid: Transcriber  config UUID.
+        :param worker_uuid: Worker config UUID, e.g. for LocalChatModule or RemoteChatModule.
+        :param synthesizer_uuid: Synthesizer config UUID.
+        :param wave_output_uuid: Wave output config UUID.
+        :param stream: Declares, whether chat model should stream its response.
+        :param forward_logging: Flag for forwarding logger to modules.
+        :param report: Flag for running report thread.
+        """
+        res = self.load_module(module_type="speech_recorder", config_uuid=UUID(speech_recorder_uuid))
+        if "error" in res:
+            return res
+        res = self.load_module(module_type="transcriber", config_uuid=UUID(transcriber_uuid))
+        if "error" in res:
+            return res
+        entry = self.database.get_object_by_id(UUID(worker_uuid))
+        if entry is not None:
+            res = self.load_module(module_type=entry[entry["module_type"]], config_uuid=UUID(entry["id"]))
+            if "error" in res:
+                return res
+        else:
+            return {"error": f"No Worker Module config with UUID '{worker_uuid}'"}
+        res = self.load_module(module_type="synthesizer", config_uuid=UUID(synthesizer_uuid))
+        if "error" in res:
+            return res
+        res = self.load_module(module_type="wave_output", config_uuid=UUID(wave_output_uuid))
+        if "error" in res:
+            return res
+
+        if self.assistant is not None:
+            self.assistant.stop()
+            self.assistant = None
+            collect()
+        self.assistant = BasicVoiceAssistant(
+            working_directory=os.path.join(cfg.PATHS.DATA_PATH, "voice_assistant"),
+            speech_recorder=self.modules["speech_recorder"],
+            transcriber=self.modules["transcriber"],
+            worker=self.modules[entry["module_type"]],
+            synthesizer=self.modules["synthesizer"],
+            wave_output=self.modules["wave_output"],
+            stream=stream,
+            forward_logging=forward_logging,
+            report=report
+        )
         self.assistant.setup()
 
-    def transcribe(self, audio_input: List[int | float] | str, dtype: str | None = None, transcription_parameters: dict | None = None) -> dict:
+    
+
+    """
+    Direct module access
+    """
+
+    def transcribe(self, 
+                   audio_input: List[int | float] | str, 
+                   dtype: str | None = None, 
+                   transcription_parameters: dict | None = None) -> dict:
         """
         Transcribes audio to text.
         :param audio_input: Audio data or wave file path.
         :param dtype: Dtype in case of audio data input.
         :param transcription_parameters: Transcription parameters as dictionary.
             Defaults to None.
+        :return: Transcript and metadata if successful, else error report.
         """
-
-        response = self.assistant.transcriber.transcribe(
-            audio_input=audio_input if isinstance(audio_input, str) else np.asanyarray(audio_input, dtype=dtype), 
-            transcription_parameters=transcription_parameters)
-        return {"transcript": response[0], "metadata": response[1]}
-
+        if isinstance(self.module_uuids["transcriber"], TranscriberModule):
+            response = self.modules["transcriber"].transcriber.transcribe(
+                audio_input=audio_input if isinstance(audio_input, str) else np.asanyarray(audio_input, dtype=dtype), 
+                transcription_parameters=transcription_parameters)
+            return {"transcript": response[0], "metadata": response[1]}
+        else:
+            return {"error": f"No active Transcriber set."}
 
     def synthesize(self, text: str, synthesis_parameters: dict | None = None) -> dict:
         """
@@ -153,7 +227,74 @@ class VoiceAssistantInterface(object):
         :param text: Text to synthesize to audio.
         :param synthesis_parameters: Synthesis parameters as dictionary.
             Defaults to None.
-        :return: Audio data, dtype and metadata.
+        :return: Audio data, dtype and metadata if successful, else error report.
         """
-        response = self.assistant.synthesizer.synthesize(text=text, synthesis_parameters=synthesis_parameters)
-        return {"synthesis": response[0].tolist(), "dtype": str(response[0].dtype), "metadata": response[1]}
+        if isinstance(self.module_uuids["synthesizer"], SynthesizerModule):
+            response = self.modules["synthesizer"].synthesizer.synthesize(
+                text=text, 
+                synthesis_parameters=synthesis_parameters)
+            return {"synthesis": response[0].tolist(), "dtype": str(response[0].dtype), "metadata": response[1]}
+        else:
+            return {"error": f"No active Synthesizer set."}
+        
+    def chat(self, 
+             prompt: str, 
+             chat_parameters: dict | None = None,
+             local: bool = True) -> dict:
+        """
+        Generates a chat response.
+        :param prompt: User input.
+        :param chat_parameters: Kwargs for chatting in the chatting process as dictionary.
+            Defaults to None in which case an empty dictionary is created.
+        :return: Generated response and metadata if successful, else error report.
+        """
+        target_worker = "local_chat" if local else "remote_chat"
+        target_worker_class = LocalChatModule if local else RemoteChatModule
+        if isinstance(target_worker, target_worker_class):
+            response = self.modules[target_worker].chat_model.chat(
+                prompt=prompt, 
+                chat_parameters=chat_parameters)
+            return {"response": response[0], "metadata": response[1]}
+        else:
+            return {"error": f"No active {self.module_titles[target_worker]} set."}
+        
+    def _wrapped_streamed_chat(self,
+                               target_worker: str,
+                               prompt: str, 
+                               chat_parameters: dict | None = None,) -> Generator[dict, None, None]:
+        """
+        Wraps a streamed chat response.
+        :param target_worker: Target worker module name.
+        :param prompt: User input.
+        :param chat_parameters: Kwargs for chatting in the chatting process as dictionary.
+            Defaults to None in which case an empty dictionary is created.
+        :return: Generated response and metadata if successful, else error report.
+        """
+        for response in self.modules[target_worker].chat_model.chat_stream(
+                prompt=prompt, 
+                chat_parameters=chat_parameters):
+            yield {"response": response[0], "metadata": response[1]}
+        
+        
+    def chat_stream(self, 
+             prompt: str, 
+             chat_parameters: dict | None = None,
+             local: bool = True) -> StreamingResponse | dict:
+        """
+        Generates a streamed chat response.
+        :param prompt: User input.
+        :param chat_parameters: Kwargs for chatting in the chatting process as dictionary.
+            Defaults to None in which case an empty dictionary is created.
+        :return: Generated response and metadata if successful, else error report.
+        """
+        target_worker = "local_chat" if local else "remote_chat"
+        target_worker_class = LocalChatModule if local else RemoteChatModule
+        if isinstance(target_worker, target_worker_class):
+            return StreamingResponse(self._wrapped_streamed_chat(
+                target_worker=target_worker,
+                prompt=prompt,
+                chat_parameters=chat_parameters
+            ))
+        else:
+            return {"error": f"No active {self.module_titles[target_worker]} set."}
+        
