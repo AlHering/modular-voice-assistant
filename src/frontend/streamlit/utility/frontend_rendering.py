e7390131e504e935ee68445e91efbf64354f3349
Alexander Hering <dev.hering@gmail.com>
2024-12-29 23:21:57 +0100

    starting flow graph prototype

--- src/frontend/streamlit/utility/frontend_rendering.py
+++ src/frontend/streamlit/utility/frontend_rendering.py
@@ -6,15 +6,14 @@
 ****************************************************
 """
 from typing import Any
-from uuid import uuid4
 import streamlit as st
-import random
 import requests
-from src.configuration import configuration as cfg
-from src.frontend.streamlit.utility.state_cache_handling import wait_for_setup
 from streamlit_flow import streamlit_flow
-from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge
+from streamlit_flow.elements import StreamlitFlowNode
 from streamlit_flow.layouts import TreeLayout
+from src.configuration import configuration as cfg
+from src.frontend.streamlit.utility.backend_interaction import AVAILABLE_MODULES, get_configs
+from src.frontend.streamlit.utility.state_cache_handling import wait_for_setup
 from streamlit_flow.state import StreamlitFlowState
 
 
@@ -63,72 +62,63 @@ def render_pipeline_node_plane(parent_widget: Any, block_dict: dict, session_sta
     :param parent_widget: Parent widget.
     :param block_dict: Dictionary for blocks.
     """
-    if 'curr_state' not in st.session_state:
-        nodes = [StreamlitFlowNode("1", (0, 0), {'content': 'Node 1'}, 'input', 'right'),
-                StreamlitFlowNode("2", (1, 0), {'content': 'Node 2'}, 'default', 'right', 'left'),
-                StreamlitFlowNode("3", (2, 0), {'content': 'Node 3'}, 'default', 'right', 'left'),
-                # StreamlitFlowNode("4", (2, 1), {'content': 'Node 4'}, 'output', target_position='left'),
-                # StreamlitFlowNode("5", (3, 0), {'content': 'Node 5'}, 'output', target_position='left'),
-                # StreamlitFlowNode("6", (3, 1), {'content': 'Node 6'}, 'output', target_position='left'),
-                # StreamlitFlowNode("7", (4, 0), {'content': 'Node 7'}, 'output', target_position='left'),
-                ]
+    if "flow" not in st.session_state:
+        st.session_state["flow"] = StreamlitFlowState(nodes=[], edges=[])
+    st.session_state["flow_modules"] = {key: {
+        "available": [entry["id"] for entry in get_configs(config_type=key)
+                       if not  entry["inactive"]],
+        "active": []
+        } for key in AVAILABLE_MODULES
+    }
+    node_menu_columns = st.columns(4)
+    
+    node_object_type = node_menu_columns[0].selectbox(
+                key=f"flow_node_object_type", 
+                label="Module type", 
+                options=list(st.session_state["flow_modules"].keys()))
+    node_object_id = node_menu_columns[1].selectbox(
+                key=f"flow_node_object_id", 
+                label="Module UUID", 
+                options=st.session_state["flow_modules"][node_object_type]["available"])
+    target_node_flow_id = f"{node_object_type}_{node_object_id}"
+    if node_menu_columns[1].button(
+        "Add", 
+        key=f"add_node_btn", 
+        disabled=node_object_id in st.session_state["flow_modules"][node_object_type]["active"]):
+        node_type = "input" if node_object_type in [
+            "speech_recorder"] else "output" if node_object_type in ["wave_output"] else "default"
 
-        edges = [StreamlitFlowEdge("1-2", "1", "2", animated=True, marker_start={}, marker_end={'type': 'arrow'}),
-                StreamlitFlowEdge("1-3", "1", "3", animated=True),
-                # StreamlitFlowEdge("2-4", "2", "4", animated=True),
-                # StreamlitFlowEdge("2-5", "2", "5", animated=True),
-                # StreamlitFlowEdge("3-6", "3", "6", animated=True),
-                # StreamlitFlowEdge("3-7", "3", "7", animated=True)
-                ]
-        
-        st.session_state.curr_state = StreamlitFlowState(nodes, edges)
+        new_node = StreamlitFlowNode(
+            id=target_node_flow_id, 
+            pos=(0, 0), 
+            data={"Type": node_object_type, "UUID": node_object_id}, 
+            node_type=node_type, 
+            source_position="right",
+            target_position="left")
+        st.session_state["flow"].nodes.append(new_node)
+        st.rerun()
 
-    col1, col2, col3, col4 = st.columns(4)
+    if node_menu_columns[1].button(
+        "Remove", 
+        key=f"remove_node_btn", 
+        disabled=node_object_id not in st.session_state["flow_modules"][node_object_type]["active"]):
 
-    with col1:
-        if st.button("Add node"):
-            new_node = StreamlitFlowNode(str(f"st-flow-node_{uuid4()}"), (0, 0), {'content': f'Node {len(st.session_state.curr_state.nodes) + 1}'}, 'default', 'right', 'left')
-            st.session_state.curr_state.nodes.append(new_node)
-            st.rerun()
+        st.session_state["flow"].nodes = [node for node in st.session_state["flow"].nodes if node.id != target_node_flow_id]
+        st.session_state["flow"].edges = [edge for edge in st.session_state["flow"].edges if edge.source != target_node_flow_id and edge.target != target_node_flow_id]
+        st.rerun()
 
-    with col2:
-        if st.button("Delete Random Node"):
-            if len(st.session_state.curr_state.nodes) > 0:
-                node_to_delete = random.choice(st.session_state.curr_state.nodes)
-                st.session_state.curr_state.nodes = [node for node in st.session_state.curr_state.nodes if node.id != node_to_delete.id]
-                st.session_state.curr_state.edges = [edge for edge in st.session_state.curr_state.edges if edge.source != node_to_delete.id and edge.target != node_to_delete.id]
-                st.rerun()
-
-    with col3:
-        if st.button("Add Edge"):
-            if len(st.session_state.curr_state.nodes) > 1:
-                source = random.choice(st.session_state.curr_state.nodes)
-                target = random.choice([node for node in st.session_state.curr_state.nodes if node.id != source.id])
-                new_edge = StreamlitFlowEdge(f"{source.id}-{target.id}", source.id, target.id, animated=True)
-                st.session_state.curr_state.edges.append(new_edge)
-                st.rerun()
-        
-    with col4:
-        if st.button("Delete Random Edge"):
-            if len(st.session_state.curr_state.edges) > 0:
-                edge_to_delete = random.choice(st.session_state.curr_state.edges)
-                st.session_state.curr_state.edges = [edge for edge in st.session_state.curr_state.edges if edge.id != edge_to_delete.id]
-                st.rerun()
-
-
-    st.session_state.curr_state = streamlit_flow('example_flow', 
-                                    st.session_state.curr_state, 
-                                    layout=TreeLayout(direction='right'), 
-                                    fit_view=True, 
-                                    height=500, 
-                                    enable_node_menu=True,
-                                    enable_edge_menu=True,
-                                    enable_pane_menu=True,
-                                    get_edge_on_click=True,
-                                    get_node_on_click=True, 
-                                    show_minimap=True, 
-                                    hide_watermark=True, 
-                                    allow_new_edges=True,
-                                    min_zoom=0.1)
-
-            
+    st.session_state["flow"] = streamlit_flow(
+        key="voice_assistant_flow", 
+        state=st.session_state["flow"], 
+        layout=TreeLayout(direction="right"), 
+        fit_view=True, 
+        height=500, 
+        enable_node_menu=True,
+        enable_edge_menu=True,
+        enable_pane_menu=True,
+        get_edge_on_click=True,
+        get_node_on_click=True, 
+        show_minimap=True, 
+        hide_watermark=True, 
+        allow_new_edges=True,
+        min_zoom=0.1)
