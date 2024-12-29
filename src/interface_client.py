f7ce84265c453d002f306019f95d91ab93a61bd1
Alexander Hering <dev.hering@gmail.com>
2024-12-29 23:31:16 +0100

    separating interface into local and remote

--- src/interface_client.py
+++ src/interface_client.py
@@ -5,11 +5,11 @@
 *            (c) 2024 Alexander Hering             *
 ****************************************************
 """
-import os
 from enum import Enum
 from typing import List, Generator
 import json
 import requests
+from src.interface import VoiceAssistantInterface
 from src.configuration import configuration as cfg
 
 
@@ -45,14 +45,14 @@ class Endpoints(str, Enum):
 
 class RemoteVoiceAssistantClient(object):
     """
-    Voice assistant client.
+    Remote voice assistant client.
     """
-    def __init__(self, working_directory: str = None) -> None:
+    def __init__(self) -> None:
         """
         Initiation method.
         :param working_directory: Working directory.
         """
-        self.working_directory = os.path.join(cfg.PATHS.DATA_PATH, "voice_assistant_client") if working_directory is None else working_directory
+        pass
 
     def check_connection(self) -> bool:
         """
@@ -269,4 +269,203 @@ class RemoteVoiceAssistantClient(object):
         except:
             pass
 
+class LocalVoiceAssistantClient(object):
+    """
+    Local voice assistant client.
+    """
+    def __init__(self) -> None:
+        """
+        Initiation method.
+        """
+        self.interface = VoiceAssistantInterface()
+
+    def check_connection(self) -> bool:
+        """
+        Checks connection to backend.
+        :return: True, if available, else False.
+        """
+        try:
+            resp = self.interface.check_connection()
+            return True
+        except Exception as ex:
+            return False
+
+    """
+    Config handling
+    """
+
+    def add_config(self,
+                   module_type: str,
+                   config: dict) -> dict | None:
+        """
+        Adds a config to the database.
+        :param module_type: Target module type.
+        :param config: Config.
+        :return: Response.
+        """
+        return self.interface.add_config(payload={
+            "module_type": module_type,
+            "config": config
+        }).get("result")
+
+    def overwrite_config(self,
+                   module_type: str,
+                   config: dict) -> dict:
+        """
+        Overwrites a config in the database.
+        :param module_type: Target module type.
+        :param config: Config.
+        :return: Response.
+        """
+        return self.interface.overwrite_config(payload={
+            "module_type": module_type,
+            "config": config
+        }).get("result")
+    
+    def get_configs(self,
+                    module_type: str = None) -> List[dict] | None:
+        """
+        Adds a config to the database.
+        :param module_type: Target module type.
+            Defaults to None in which case all configs are returned.
+        :return: Response.
+        """
+        return self.interface.get_configs(payload={"module_type": module_type}).get("result")
+
+    """
+    Module handling
+    """
+
+    def load_module(self,
+                    module_type: str,
+                    config_uuid: str) -> dict:
+        """
+        Loads a module from the given config UUID.
+        :param module_type: Target module type.
+        :param config_uuid: Config UUID.
+        :return: Response.
+        """
+        return self.interface.load_module(payload={
+            "module_type": module_type,
+            "config_uuid": config_uuid
+        })
+            
+    def unload_module(self,
+                      module_type: str,
+                      config_uuid: str) -> dict:
+        """
+        Unloads a module from the given config UUID.
+        :param module_type: Target module type.
+        :param config_uuid: Config UUID.
+        :return: Response.
+        """
+        self.interface.unload_module(payload={
+            "module_type": module_type,
+            "config_uuid": config_uuid
+        })
+
+    """
+    Assistant handling
+    """
+
+    def setup_assistant(self,
+                        speech_recorder_uuid: str,
+                        transcriber_uuid: str,
+                        worker_uuid: str,
+                        synthesizer_uuid: str,
+                        wave_output_uuid: str,
+                        stream: bool = True,
+                        forward_logging: bool = False,
+                        report: bool = False) -> dict:
+        """
+        Sets up a voice assistant from currently configured modules.
+        :param speech_recorder_uuid: Speech Recorder config UUID.
+        :param transcriber_uuid: Transcriber  config UUID.
+        :param worker_uuid: Worker config UUID, e.g. for LocalChatModule or RemoteChatModule.
+        :param synthesizer_uuid: Synthesizer config UUID.
+        :param wave_output_uuid: Wave output config UUID.
+        :param stream: Declares, whether chat model should stream its response.
+        :param forward_logging: Flag for forwarding logger to modules.
+        :param report: Flag for running report thread.
+        """
+        return self.interface.setup_assistant(payload={
+            "speech_recorder_uuid": speech_recorder_uuid,
+            "transcriber_uuid": transcriber_uuid,
+            "worker_uuid": worker_uuid,
+            "synthesizer_uuid": synthesizer_uuid,
+            "wave_output_uuid": wave_output_uuid,
+            "stream": stream,
+            "forward_logging": forward_logging,
+            "report": report
+        })
+
+    """
+    Direct module access
+    """
+
+    def transcribe(self, 
+                   audio_input: List[int | float] | str, 
+                   dtype: str | None = None, 
+                   transcription_parameters: dict | None = None) -> dict:
+        """
+        Transcribes audio to text.
+        :param audio_input: Audio data or wave file path.
+        :param dtype: Dtype in case of audio data input.
+        :param transcription_parameters: Transcription parameters as dictionary.
+            Defaults to None.
+        :return: Transcript and metadata if successful, else error report.
+        """
+        return self.interface.transcribe(**{
+            "audio_input": audio_input,
+            "dtype": dtype,
+            "transcription_parameters": transcription_parameters,
+        })
+
+    def synthesize(self, text: str, synthesis_parameters: dict | None = None) -> dict:
+        """
+        Synthesizes audio from input text.
+        :param text: Text to synthesize to audio.
+        :param synthesis_parameters: Synthesis parameters as dictionary.
+            Defaults to None.
+        :return: Audio data, dtype and metadata if successful, else error report.
+        """
+        return self.interface.synthesize(**{
+            "text": text,
+            "synthesis_parameters": synthesis_parameters,
+        })
+        
+    def chat(self, 
+             prompt: str, 
+             chat_parameters: dict | None = None,
+             local: bool = True) -> dict:
+        """
+        Generates a chat response.
+        :param prompt: User input.
+        :param chat_parameters: Kwargs for chatting in the chatting process as dictionary.
+            Defaults to None in which case an empty dictionary is created.
+        :return: Generated response and metadata if successful, else error report.
+        """
+        return self.interface.chat(**{
+            "prompt": prompt,
+            "chat_parameters": chat_parameters,
+            "local": local,
+        })
+        
+    def chat_stream(self, 
+             prompt: str, 
+             chat_parameters: dict | None = None,
+             local: bool = True) -> Generator[dict, None, None] | None:
+        """
+        Generates a streamed chat response.
+        :param prompt: User input.
+        :param chat_parameters: Kwargs for chatting in the chatting process as dictionary.
+            Defaults to None in which case an empty dictionary is created.
+        :return: Generated response and metadata if successful, else error report.
+        """
+        for response in self.interface._wrapped_streamed_chat(**{
+            "prompt": prompt,
+            "chat_parameters": chat_parameters,
+            "local": local
+        }):
+            yield response
         
