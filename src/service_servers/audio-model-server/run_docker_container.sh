02d2645712580a50b462d0ac9d97e9bfd451a34f
Alexander Hering <dev.hering@gmail.com>
2025-02-18 13:32:20 +0100

    scrapping audio server extraction

--- src/service_servers/audio-model-server/run_docker_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run  \
-    -it --net=host --gpus all \
-    --mount type=bind,source="$1",target=/service/models \
-    --mount type=bind,source="$2",target=/service/configs \
-    "audio-model-server:v0.1" /bin/bash
