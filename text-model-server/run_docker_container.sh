d1c73d1d5acd3a6ac232f0869619424548290791
Alexander Hering <dev.hering@gmail.com>
2025-06-15 22:00:27 +0200

    parameterized image name

--- text-model-server/run_docker_container.sh
+++ text-model-server/run_docker_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run  \
     -it --net=host --gpus all \
-    --mount type=bind,source="$1",target=/service/models \
-    --mount type=bind,source="$2",target=/service/configs \
-    "text-model-server:v0.3" /bin/bash
+    --mount type=bind,source="$2",target=/service/models \
+    --mount type=bind,source="$3",target=/service/configs \
+    "$1" /bin/bash
